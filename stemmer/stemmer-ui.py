321a7af083e12f7ba182777e5d5e588ec20fb4f4
loghi <loghijiaha.16@cse.mrt.ac.lk>
2019-01-07 00:37:13 +0530

    tamil stmmer.py for each word with the help of stemming from snowballstemmer

---
+++ stemmer/stemmer-ui.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+
+import subprocess
+def stemWord(str):
+    # process= subprocess.Popen(("./stemwords", "-l", "ta"), stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.STDOUT)
+    # out = process.communicate(input='கண்கள்\n')[0]
+    # return out.decode()
+    p = subprocess.run(("./stemwords", "-l", "ta"), stdout=subprocess.PIPE,
+            input=str, encoding='utf-8')
+    return (p.stdout)
+
+
